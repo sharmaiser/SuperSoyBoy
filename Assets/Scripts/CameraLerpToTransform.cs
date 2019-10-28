7c41ff91ba3b08a8fde40206bea02d5c382757a9
Bhavay Sharma [Student] <bhavay.sharma@student.sl.on.ca>
2019-10-28 15:21:56 -0400

    Camera Configured
    SoyBoy is now being followed by the Camera.
    Camera script component aslo added.

--- Assets/Scripts/CameraLerpToTransform.cs
+++ Assets/Scripts/CameraLerpToTransform.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class CameraLerpToTransform : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // 1 
+    public Transform camTarget;
+    public float trackingSpeed;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+    // 2
+    void FixedUpdate()
+    {
+        if (camTarget != null)
+        {
+            var newPos = Vector2.Lerp(transform.position,
+                camTarget.position,
+                Time.deltaTime * trackingSpeed);
+            var camPosition = new Vector3(newPos.x, newPos.y, -10f);
+            var v3 = camPosition;
+            var clampX = Mathf.Clamp(v3.x, minX, maxX);
+            var clampY = Mathf.Clamp(v3.y, minY, maxY);
+            transform.position = new Vector3(clampX, clampY, -10f);
+        }
+    }
 }
