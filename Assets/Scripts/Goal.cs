4ae52377d95468bc7955d3cf87916635aa0d2ad2
sharmaiser <54967627+sharmaiser@users.noreply.github.com>
2019-11-21 23:09:27 -0500

    Chapter 13 Goal Added!

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public AudioClip goalClip;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && goalClip != null)
+            {
+                audioSource.PlayOneShot(goalClip);
+            }
+            GameManager.instance.RestartLevel(0.5f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
